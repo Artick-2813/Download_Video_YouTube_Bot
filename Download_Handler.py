b23fd868a151b1a1e5bf683f18a7899b8ca01c9a
Artem <super.alshan@mail.ru>
2022-01-19 14:07:35 +0300

    version 0.0.2
    I wrote the code through the OOP, added new methods with which you can get the name of the video and url.

--- Download_Handler.py
+++ Download_Handler.py
@@ -1,29 +1,62 @@
 from pytube import YouTube
 
 
-def download_video(url, quality):
-    try:
+class DownloadVideoFromYouTube:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_title_video(self):
+        try:
+            url = self.url
+            yt = YouTube(url)
+            title = yt.title
+            return title
+
+        except Exception as e:
+            print('ERROR:', str(e))
+            return 'Неправильный ввод url адреса ❗'
+
+    def get_url_video(self):
+        try:
+            url_video = self.url
+            return url_video
+        except Exception as e:
+            print('ERROR:', str(e))
+            return 'Не удалось получить URL адрес ❗'
+
+    def download_video_720p(self, quality):
+        url = self.url
         yt = YouTube(url)
         PATH = r'C:\Users\Admin\Desktop\Download_video'
 
         if quality == '720p':
             stream = yt.streams.get_by_itag(22)
-            if not stream:
-                return 'Такого разрешения не существует ❗'
-
-            else:
-                stream.download(output_path=PATH)
-                return 'Видео загружено ✅'
-
-        if quality == '480p':
-            stream = yt.streams.get_by_itag(135)
-            if not stream:
-                return 'Такого разрешения не существует ❗'
-            else:
-                stream.download(output_path=PATH)
-                return 'Видео загружено ✅'
+            stream.download(output_path=PATH)
+            return 'Видео загружено ✅'
         else:
-            return 'Укажите разрешения видео ❗'
-    except Exception as e:
-        print('ERROR:', str(e))
-        return 'Неправильный ввод url адреса ❗'
+            return 'Вы выбрали неправильное разрешение видео ❗'
+
+    def download_video_360p(self, quality):
+        url = self.url
+        yt = YouTube(url)
+        PATH = r'C:\Users\Admin\Desktop\Download_video'
+
+        if quality == '360p':
+            stream = yt.streams.get_by_itag(18)
+            stream.download(output_path=PATH)
+            return 'Видео загружено ✅'
+        else:
+            return 'Вы выбрали неправильное разрешение видео ❗'
+
+    def download_video_144p(self, quality):
+        url = self.url
+        yt = YouTube(url)
+        PATH = r'C:\Users\Admin\Desktop\Download_video'
+
+        if quality == '144p':
+            stream = yt.streams.get_by_itag(17)
+            stream.download(output_path=PATH)
+            return 'Видео загружено ✅'
+        else:
+            return 'Вы выбрали неправильное разрешение видео ❗'
