d119e00fc133c023f3254d6d65b6160cdabf532f
Artem <super.alshan@mail.ru>
2022-04-01 22:08:01 +0300

    version 0.0.6
    Adding audio downloads to the bot

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ from create_bot import dp
 
 logging.basicConfig(level=logging.INFO)
 
-from HandlersBot import start, download_video
+from HandlersBot import start, download_video, download_audio
 
 start.register_handler_start(dp)
 download_video.register_handlers_download_video(dp)
+download_audio.register_handlers_download_audio(dp)
 
 
 executor.start_polling(dp, skip_updates=True)
